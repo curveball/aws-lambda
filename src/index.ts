bd359a087960c39e105fb906cb40082889c9bd7b
Evert Pot <me@evertpot.com>
2019-05-22 18:32:40 -0400

    First real commit.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-/* tslint:disable */
-console.log('Hello world!');
+import { Application, Context, MemoryRequest, MemoryResponse } from '@curveball/core';
+import qs from 'querystring';
+import { AwsLambdaHandler, AwsRequest, AwsResponse } from './types';
+import { convertBody, convertHeaders } from './util';
+
+export default function lambdaHandler(app: Application): AwsLambdaHandler {
+
+  return async (awsRequest: AwsRequest): Promise<AwsResponse> => {
+
+    const request = new MemoryRequest(
+      awsRequest.httpMethod,
+      awsRequest.path + '?' + qs.stringify(awsRequest.multiValueQueryStringParameters),
+      awsRequest.headers,
+      awsRequest.isBase64Encoded ? Buffer.from(awsRequest.body, 'base64') : awsRequest.body,
+    );
+    const response = new MemoryResponse();
+    const context = new Context(request, response);
+
+    await app.handle(context);
+
+    const [isBase64Encoded, body] = convertBody(context.response);
+
+    return {
+      isBase64Encoded,
+      statusCode: context.response.status,
+      headers: {},
+      multiValueHeaders: convertHeaders(context.response),
+      body
+    };
+
+  };
+
+}
+
---
+++ src/util.ts
@@ -0,0 +1,27 @@
+import { Response } from '@curveball/core';
+import { KeyMultiValue } from './types';
+
+export function convertHeaders(response: Response): KeyMultiValue {
+
+  const result: KeyMultiValue = {};
+  for (const [key, value] of Object.entries(response.headers.getAll())) {
+    const newVal = Array.isArray(value) ? value : [value];
+    result[key] = newVal.map( item => typeof item === 'number' ? item.toString() : item );
+  }
+  return result;
+
+}
+
+export function convertBody(response: Response): [boolean, string] {
+
+  if (response.body === undefined || response.body === null) {
+    return [false, ''];
+  } else if (typeof response.body === 'string') {
+    return [false, response.body];
+  } else if (response.body instanceof Buffer) {
+    return [true, response.body.toString('base64')];
+  } else {
+    return [false, JSON.stringify(response.body)];
+  }
+
+}
